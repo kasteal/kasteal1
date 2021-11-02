f59ae3fbf5851f8ae5bf07162498b82424cab05a
kasteal <90476805+kasteal@users.noreply.github.com>
2021-11-03 01:45:38 +1000

    Add files via upload

--- rocket2.py
+++ rocket2.py
@@ -6,54 +6,56 @@ MODEL_G = 9.81
 
 
 class Body:
-    def __init__(self, x, vx):
+    def __init__(self, x, y, vx, vy):
         self.x = x
+        self.y = y
         self.vx = vx
+        self.vy = vy
+
         self.sx = []
-    def up(self):
+        self.sy = []
+
+    def advance(self):
         self.sx.append(self.x)
+        self.sy.append(self.y)
+
         self.x += self.vx * MODEL_DT
+        self.y += self.vy * MODEL_DT
+        self.vy -= MODEL_G * MODEL_DT
+
 
 
 
 class Rocket(Body):
     def __init__(self, x, y, m, u, vx):
-        self.x = x
-        self.y = y
-        self.sx = []
-        self.sy = []
+        super().__init__(x, y, vx, 20)
         self.m0 = m
         self.m = m
-        self.vx = vx
-        self.vy = 4
         self.u = u
-
-    def up(self):
-
-        self.sx.append(self.x)
-        self.sy.append(self.y)
-        self.x += self.vx * MODEL_DT
-        self.vy += self.u * math.log(self.m0/self.m)
-        self.y += self.vy
-        self.m -= self.m0 * (1 / 2000)
+    def advance(self):
+        super().advance()
+        self.vy += self.u * MODEL_DT
+        self.m -= self.m0 * MODEL_DT
         if self.m <= 0:
             self.padaem()
 
 
     def padaem(self):
         while self.y > 0:
-            self.x += self.vx * MODEL_DT
-            self.vy -= MODEL_G * MODEL_DT
-            self.y += self.vy
             if self.y < 0:
                 self.y = 0
-            self.sy.append(self.y)
-            self.sx.append(self.x)
+            super().advance()
 
 
-a = Rocket(200, 100, 15, 0.001, 50)
+a = Rocket(200, 0, 15, 40, 50)
+b = Body(200, 100, 1, 2)
+
 while a.m > 0:
-    a.up()
+    a.advance()
+while b.y > 0:
+    b.advance()
 
 mpp.plot(a.sx, a.sy)
+mpp.show()
+mpp.plot(b.sx, b.sy)
 mpp.show()
