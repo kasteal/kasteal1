c9dd0188c72a0cfdc085a9b7f5040e5c68f4197c
kasteal <90476805+kasteal@users.noreply.github.com>
2021-11-04 06:21:59 +1000

    Add files via upload

---
+++ herepaha.py
@@ -0,0 +1,9 @@
+#  def up(self):
+#     while self.m > 0:
+#        self.sx.append(self.x)
+#      self.sy.append(self.y)
+#     self.x += self.vx * MODEL_DT
+#    self.vy += self.u * math.log(self.m0/self.m) *MODEL_DT
+#   self.y += self.vy
+#  self.m -= self.m0 * (1 / 2000)
+#   self.padaem()
