dbd00a61c2fe5ab9adbea74d1a5270348562334b
kasteal <90476805+kasteal@users.noreply.github.com>
2021-11-04 05:06:26 +1000

    Add files via upload

--- rocket2.py
+++ rocket2.py
@@ -6,56 +6,51 @@ MODEL_G = 9.81
 
 
 class Body:
-    def __init__(self, x, y, vx, vy):
+    def __init__(self, x, y, vx, vy, t):
         self.x = x
         self.y = y
+        self.t = t
         self.vx = vx
         self.vy = vy
-
         self.sx = []
         self.sy = []
 
     def advance(self):
-        self.sx.append(self.x)
-        self.sy.append(self.y)
-
-        self.x += self.vx * MODEL_DT
-        self.y += self.vy * MODEL_DT
-        self.vy -= MODEL_G * MODEL_DT
-
+        self.t += MODEL_DT
+        if self.y > 0:
+            self.sx.append(self.x)
+            self.sy.append(self.y)
+            self.x += self.vx * MODEL_DT
+            self.y += self.vy * MODEL_DT
+            self.vy -= MODEL_G * MODEL_DT
 
 
 
 class Rocket(Body):
-    def __init__(self, x, y, m, u, vx):
-        super().__init__(x, y, vx, 20)
+    def __init__(self, x, y, vx, vy, u, m, dm, t):
+        super().__init__(x, y, vx, vy, t)
         self.m0 = m
         self.m = m
         self.u = u
+        self.dm = dm
+
     def advance(self):
         super().advance()
-        self.vy += self.u * MODEL_DT
-        self.m -= self.m0 * MODEL_DT
-        if self.m <= 0:
-            self.padaem()
+        if self.m > 0 and 2 > self.t > 1:
+            self.vy -= self.u * MODEL_DT
+            self.m -= self.dm * MODEL_DT
+        elif self.m > 0:
+            self.vy += self.u * MODEL_DT
+            self.m -= self.dm * MODEL_DT
 
+a = Rocket(200, 11, 10, 20, 40, 20, 5, 0)
+b = Body(200, 1, 10, 90, 0)
 
-    def padaem(self):
-        while self.y > 0:
-            if self.y < 0:
-                self.y = 0
-            super().advance()
-
-
-a = Rocket(200, 0, 15, 40, 50)
-b = Body(200, 100, 1, 2)
-
-while a.m > 0:
+while a.t < 20:
     a.advance()
-while b.y > 0:
     b.advance()
+    print(a.t)
 
 mpp.plot(a.sx, a.sy)
-mpp.show()
 mpp.plot(b.sx, b.sy)
 mpp.show()
