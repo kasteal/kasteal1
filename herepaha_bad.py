f6e0ecf4fe26e84d07b72174175f1e7428d25f09
kasteal <90476805+kasteal@users.noreply.github.com>
2021-11-05 15:55:51 +0300

    Add files via upload

---
+++ herepaha_bad.py
@@ -0,0 +1,21 @@
+import turtle as tl
+
+
+def dra_squar(scale):
+    if scale > 0:
+        tl.up()
+        tl.forward(5)
+        tl.left(90)
+        tl.forward(5)
+        tl.down()
+        tl.right(90)
+        for i in range(4):
+            tl.forward(scale)
+            tl.left(90)
+        dra_squar(scale-10)
+
+
+scal = 400
+
+dra_squar(scal)
+tl.done()
