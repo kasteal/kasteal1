0fca68b0ce3a6dcb7ba413e7d5888c27c93ef66d
kasteal <90476805+kasteal@users.noreply.github.com>
2022-06-06 04:06:20 +0300

    Add files via upload

--- web cam/qq.py
+++ web cam/qq.py
@@ -72,7 +72,7 @@ def gaus(ans='Решений нет'):
         x = gauss(a,b)
         ans = f'x1 = {x[0]}, x2 = {x[1]}, x3 = {x[2]}'
     else:
-        ans = 'Решений нет'
+        ans = 'hui'
     return render_template('gaus.html', ans=ans)
 
 def no_mt(a):
